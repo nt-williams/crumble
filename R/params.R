2d9bd11bd3da0c37fceef4e4bd71f9384a19f792
nt-williams <ntwilliams.personal@gmail.com>
2024-07-09 11:51:09 -0700

    Make param lists

---
+++ R/params.R
@@ -0,0 +1,49 @@
+natural <- list(
+	natural = list(
+		c("1", "1", "1"),
+		c("1", "0", "0"),
+		c("0", "0", "0")
+	),
+	randomized = list()
+)
+
+organic <- list(
+	natural = list(
+		c("1", "0", "1"),
+		c("0", "0", "0"),
+		c("1", "1", "1")
+	),
+	randomized = list()
+)
+
+decision <- list(
+	natural = list(
+		c("1", "0", "0"),
+		c("0", "0", "0"),
+		c("1", "1", "1")
+	),
+	randomized = list()
+)
+
+randomized <- list(
+	natural = list(),
+	randomized = list(
+		c("1", "1", "0", "0"),
+		c("0", "0", "0", "0"),
+		c("1", "1", "1", "1"),
+	)
+)
+
+recanting_twin <- list(
+	natural = list(
+		c("1", "1", "1"),
+		c("0", "1", "1"),
+		c("0", "1", "0"),
+		c("0", "0", "0")
+	),
+	randomized = list(
+		c("0", "0", "1", "1"),
+		c("0", "0", "1", "0"),
+		c("0", "1", "1", "1")
+	)
+)
